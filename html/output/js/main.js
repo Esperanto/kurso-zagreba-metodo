e73321ac5256f1f988c88d6ef13609db298f7d82
Georg Jaehnig <georg@jaehnig.org>
2016-01-14 18:33:40 +0100

    Validate.

--- html/output/js/main.js
+++ html/output/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+$('input[data-expected]').keyup(function() {
+  var formGroup = $(this).closest('.form-group');
+  var glyphicon = $(this).next('span.glyphicon');
+  if ( $(this).val() == $(this).attr('data-expected') ) {
+    formGroup.removeClass('has-error').addClass('has-success');
+    glyphicon.removeClass('glyphicon-remove').addClass('glyphicon-ok');
+  } else {
+    formGroup.removeClass('has-success').addClass('has-error');
+    glyphicon.removeClass('glyphicon-ok').addClass('glyphicon-remove');
+  }
+});
+
+
+

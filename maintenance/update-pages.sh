c39a6111c14a2383d9efbb4d32ec0d61d488a1c7
Georg Jaehnig <georg@jaehnig.org>
2016-05-15 14:33:08 +0200

    Ne kreu plu por github.com.

--- maintenance/update-pages.sh
+++ maintenance/update-pages.sh
@@ -1,16 +1,6 @@
 git push
 
 # Create pages for
-# http://esperanto.github.io/kurso-zagreba-metodo/html/output/
-git checkout gh-pages
-git merge master -m "Merge from master."
-python generate.py /kurso-zagreba-metodo/html/output/
-git add -f html/output/*
-git commit -m "Updated output for gh-pages."
-git push origin gh-pages
-git checkout master
-
-# Create pages for
 # http://learn.esperanto.com/
 git checkout root-pages
 git merge master -m "Merge from master."

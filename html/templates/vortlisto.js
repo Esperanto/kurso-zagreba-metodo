f7c33629a20983bd92508aa4303e20048cd1ea1a
Georg Jaehnig <georg@jaehnig.org>
2017-03-21 01:25:47 +0100

    Indent.

--- html/templates/vortlisto.js
+++ html/templates/vortlisto.js
@@ -1,23 +1,23 @@
 var vortlisto = {
-	'esperante': [
-		{% for esperante in enhavo.vortaro|sort -%}
-			"{{esperante}}
+  'esperante': [
+    {% for esperante in enhavo.vortaro|sort -%}
+      "{{esperante}}
       {%- if esperante|lower in enhavo.finajxoj -%}
-	      {{enhavo.finajxoj[esperante]}}
+        {{enhavo.finajxoj[esperante]}}
       {%- endif -%} 
       "
-			{%- if not loop.last -%}
-				,
-			{%- endif -%}
-		{%- endfor -%}
-	], 
-	'fontlingve': [
-		{%- for esperante in enhavo.vortaro|sort -%}
-			{%- set klavo = esperante -%}
-			"{%- include 'tradukajxo.html' -%}"
-			{%- if not loop.last -%}
-				,
-			{%- endif -%}
-		{%- endfor -%}
-	]
+      {%- if not loop.last -%}
+        ,
+      {%- endif -%}
+    {%- endfor -%}
+  ], 
+  'fontlingve': [
+    {%- for esperante in enhavo.vortaro|sort -%}
+      {%- set klavo = esperante -%}
+      "{%- include 'tradukajxo.html' -%}"
+      {%- if not loop.last -%}
+        ,
+      {%- endif -%}
+    {%- endfor -%}
+  ]
 };

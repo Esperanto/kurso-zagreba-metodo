60d3c71021e484df48670bf3bd167348017696e1
Joop Kiefte <ikojba@gmail.com>
2018-08-30 02:30:58 +0200

    Korekti nomon de la skripto, korekti komenton ene de ĝi.

--- create-baselink.sh
+++ create-baselink.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Ĉi tiu skripto kreas ĉiujn paĝojn kaj reskribas ilin tiel ke ili povu esti uzataj en ekzemple
-# Github Pages kaj ipfs sen aldonaj ŝanĝoj. La celo de ĉi tio estas faciligi testadon de postaj
-# ŝanĝoj, kiel ekzemple la implementado de la PWA-dosieroj.
+# Ĉi tiu skripto kreas ĉiujn paĝojn kaj eventuale reskribas ilin por Github Pages.
+# Alvoku kiel ./create-baselink.sh https://esperanto.github.io/kurso-zagreba-metodo/
 git stash
 git reset --hard
 git stash apply

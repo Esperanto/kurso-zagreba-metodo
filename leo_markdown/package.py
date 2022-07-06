0c93a0d32ad3f0d5b216134467d87ac718da8e6e
Georg Jähnig <georg@jaehnig.org>
2022-07-06 02:04:07 +0200

    Use suggestions from linter.

--- leo_markdown/package.py
+++ leo_markdown/package.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import re
-import jinja2, mistune
+
+import jinja2
+import mistune
+
 
 def kreu_md(enhavo, printendaj):
-  md = mistune.Markdown()
+    md = mistune.Markdown()
 
-  env = jinja2.Environment()
-  env.filters['markdown'] = lambda text: jinja2.Markup(md(text))
-  env.trim_blocks = True
-  env.lstrip_blocks = True
-  env.loader=jinja2.FileSystemLoader('leo_markdown/templates/')
+    env = jinja2.Environment()
+    env.filters['markdown'] = lambda text: jinja2.Markup(md(text))
+    env.trim_blocks = True
+    env.lstrip_blocks = True
+    env.loader = jinja2.FileSystemLoader('leo_markdown/templates/')
 
-  # Ŝanĝu __ al **, ĉar nur tio Pandoc ŝajne komprenas.
-  for i in range(len(enhavo['lecionoj'])):
-      enhavo['lecionoj'][i]['gramatiko']['teksto'] = re.sub('__', '**', enhavo['lecionoj'][i]['gramatiko']['teksto'] )
+    # Ŝanĝu __ al **, ĉar nur tio Pandoc ŝajne komprenas.
+    for i in range(len(enhavo['lecionoj'])):
+        enhavo['lecionoj'][i]['gramatiko']['teksto'] = re.sub('__', '**', enhavo['lecionoj'][i]['gramatiko']['teksto'])
 
-  rendered = env.get_template('arangxo.md').render(enhavo = enhavo, printendaj = printendaj)
-  print(rendered)
+    rendered = env.get_template('arangxo.md').render(enhavo=enhavo, printendaj=printendaj)
+    print(rendered)

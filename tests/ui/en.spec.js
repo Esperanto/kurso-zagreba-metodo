217c976eae75920faa335ab285dacedb0b8bda85
Georg Jähnig <georg@jaehnig.org>
2026-07-09 14:38:24 +0200

    Anstataŭigu komentejan butonon per helpo

--- tests/ui/en.spec.js
+++ tests/ui/en.spec.js
@@ -209,13 +209,17 @@ test('malsupra pagxilo havas apartigan linion', async ({ page }) => {
   await expect(page.locator('ul.pager')).toHaveCSS('border-top-style', 'solid');
 });
 
-test('komentoj malfermigxas kiel ekstera ligilo', async ({ page }) => {
+test('helpo malfermigxas kiel ekstera ligilo', async ({ page }) => {
   await page.goto('/en/01/');
 
-  const commentsLink = page.locator('ul.pager .comments a');
-  await expect(commentsLink).toHaveAttribute('href', 'https://kzm-en.disqus.com/01/');
-  await expect(commentsLink).toHaveAttribute('target', '_blank');
-  await expect(commentsLink).toHaveAttribute('rel', 'noopener');
+  const helpLink = page.locator('ul.pager .help a');
+  await expect(helpLink).toHaveAttribute(
+    'href',
+    'https://www.google.com/search?hl=en&udm=50&aep=11&q=Estu+babilroboto+por+la+enhavo+de+responde+al+miaj+demandoj+pri+https%3A%2F%2Fesperanto12.net%2Fen%2Fllms-full.txt.+Skribu+bonvenigan+mesa%C4%9Don+por+tio+en+%5Ben%5D',
+  );
+  await expect(helpLink).toHaveAttribute('target', '_blank');
+  await expect(helpLink).toHaveAttribute('rel', 'noopener');
+  await expect(page.locator('ul.pager .comments a')).toHaveCount(0);
   await expect(page.locator('#disqus_thread')).toHaveCount(0);
   await expect(page.locator('script[src*="disqus.com/embed.js"]')).toHaveCount(0);
 });
